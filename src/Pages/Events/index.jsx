bc526670ea748ead9778c82962d2bb21c06ebd4b
VaishaliAgg14 <vaishali.aggarwal1410@gmail.com>
2022-06-28 22:39:13 +0530

    events_commit1

--- src/Pages/Events/index.jsx
+++ src/Pages/Events/index.jsx
@@ -1,7 +1,23 @@
-import "./events.css";
+import React from 'react';
+import Card from './card';
+import styles from './events.module.css';
 
-function Events() {
-  return <div>events</div>;
+const Events = () => {
+  return (
+    <div>
+    <h1>Events and Workshops</h1>
+    <div className={styles.eventsContainer}>
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+  </div>
+  </div>
+  )
 }
 
-export default Events;
+export default Events
