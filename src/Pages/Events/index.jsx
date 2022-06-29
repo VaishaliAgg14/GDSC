6df0532a4e06348e21f3cac9365b1dcc5de034cb
Dhruv <dhruvdeora31@gmail.com>
2022-06-30 00:32:51 +0530

    Perfected the Cards

--- src/Pages/Events/index.jsx
+++ src/Pages/Events/index.jsx
@@ -1,25 +1,35 @@
-import React from 'react';
-import Card from './card';
-import styles from './events.module.css';
+import React from "react";
+import Card from "./card";
+import styles from "./events.module.css";
 
+import eve from "./events_upcoming.json";
 const Events = () => {
   return (
     <div>
-    <h1>Events and Workshops</h1>
-    <div className={styles.eventsContainer}>
-    <Card />
-    <Card />
-    <Card />
-    <Card />
-    <Card />
-    <Card />
-    <Card />
-    <Card />
-    <Card />
-    <Card />
-  </div>
-  </div>
-  )
-}
+      <h1 className={styles.super}>Events and Workshops</h1>
+      <div className={styles.eventsContainer}>
+        {eve.map((even) => {
+          return (
+            <>
+              <Card
+                key={even.id}
+                date={even.date}
+                venue={even.venue}
+                name={even.name}
+              />
+            </>
+          );
+        })}
 
-export default Events
+        {/* <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card /> */}
+      </div>
+    </div>
+  );
+};
+
+export default Events;
