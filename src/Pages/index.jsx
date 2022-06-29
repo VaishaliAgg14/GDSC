dcd62757ba5523ee4998d4aba8cd7412db22e4aa
VaishaliAgg14 <vaishali.aggarwal1410@gmail.com>
2022-06-29 23:08:19 +0530

    first-commit

---
+++ src/Pages/index.jsx
@@ -0,0 +1,7 @@
+import Home from "./Home";
+import Team from "./Team";
+import Feed from "./Feed";
+import Events from "./Events";
+import Contact from "./Contact";
+
+export { Home, Team, Feed, Events, Contact };
