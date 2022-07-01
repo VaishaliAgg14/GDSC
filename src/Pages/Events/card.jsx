845aa4d9a3ca1f9b4a208bc52041c3f19a127449
VaishaliAgg14 <vaishali.aggarwal1410@gmail.com>
2022-07-01 09:23:31 +0530

    Added sections

--- src/Pages/Events/card.jsx
+++ src/Pages/Events/card.jsx
@@ -1,41 +1,46 @@
-import React from 'react'
+import React from "react";
 import styles from "./events.module.css";
-import image from "../../Assets/Images/image1_events.jfif";
+import image from "../../Assets/Images/hello.png";
+import bg from "../../Assets/Images/bg.png";
 // import { IoLocation } from "react-icons/io";
 import { MdLocationPin } from "react-icons/md";
 // import {IoTicket} from 'react-icons/io';
-import {GiTicket} from 'react-icons/gi';
+import { AiTwotoneCalendar } from "react-icons/ai";
 
-
-const Card = () => {
+const Card = ({ date, venue, name }) => {
   return (
     <div className={styles.container}>
-    <div>
-      <img src={image} alt="" />
-    </div>
-    <div>
-      <div className={styles.heading}>Google Kickstart Workshop</div>
-      <div className={styles.date}>18 August 2022</div>
-      <div className={styles.location}>
-        <div className={styles.locationIcon}>
-          <MdLocationPin size={25} />
+      <div>
+        <img src={image} alt="" />
+      </div>
+      <div>
+        <div className={styles.content}>
+          <div className={styles.heading}>{name}</div>
+          {/* <div className={styles.date}>18 August 2022</div> */}
+          <div className={styles.location}>
+            <div className={styles.locationIcon}>
+              <MdLocationPin className={styles.pin} size={20} />
+            </div>
+            <div className={styles.locationContent}>{venue}</div>
+          </div>
+          <div className={styles.ticket}>
+            <div className={styles.ticketIcon}>
+              <AiTwotoneCalendar className={styles.calendar} size={20} />
+            </div>
+            <div className={styles.ticketContent}>{date}</div>
+          </div>
+          <div className={styles.buttonContainer}>
+            <button className={styles.button1}>
+              <div className={styles.buttonText}>Join</div>
+            </button>
+            <button className={styles.button2}>
+              <div className={styles.buttonText2}>Details</div>
+            </button>
+          </div>
         </div>
-        <div className={styles.locationContent}> Main Auditorium , Admin Block</div>
-      </div>
-      <div className={styles.ticket}>
-        <div className={styles.ticketIcon}>
-          <GiTicket size={25} />
-        </div>
-        <div className={styles.ticketContent}>$10.99</div>
-      </div>
-      <div className={styles.buttonContainer}>
-      <button>
-        <div className={styles.buttonText}>Join</div>
-      </button>
       </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
